2ecd006888a01f18a3efbcfea5f612881da7e795
Riajul Islam <riajulpislam@gmail.com>
2023-12-08 18:13:38 +0600

    scope

--- Function/6Scope/Block_scope-vs-Function_scope.js
+++ Function/6Scope/Block_scope-vs-Function_scope.js
@@ -1,19 +1,92 @@
-//Block scope VS function scope
-// function myName(){
-//     var name = 'riajul islam';
+//Block scope vs function scope
+
+//let and const are block scope
+//var is function scope
+
+// {
+    //block
+// }
+
+
+//alada alada block  let use
+// {
+//     let firstName = 'riajul';
+//     console.log(firstName)
+// }
+// {
+//     let firstName = 'islam';
+//     console.log(firstName)
+// }
+// {
+//     console.log(firstName) // Error ashbe
+// }
+// let firstName = 'pavel';
+// console.log(firstName)
+
+
+
+//const use
+// {
+//     const lastname = 'pavel';
+//     console.log(lastname)
+// }
+// {
+//    const  lastname = 'fahim';
+//     console.log(lastname)
+// }
+// {
+//     console.log(lastname)  //Error ashbe 
+// }
+
+
+
+//var is function scope
+
+// {
+//     var myName = 'riajul';
+// }
+// console.log(myName) //  bahere accsess 
+
+// {
+//     var myName = 'islam';
+//     console.log(myName)
+// }
+// {
+//     console.log(myName)  //true
+// }
+
+//local scope
+// function name(){
+//     var myname = 'riajul';
+//     console.log(myname)
+// }
+// name()
+
+
+//global scope
+// let myName = 'riajul'
+// function name(){
+//     console.log("inside a variable"+myName)
+// }
+// name()
+// console.log('outside a variable'+myName)
+
+
+// if(true){
+//     var name = 'riajul';
 //     console.log(name)
 // }
-// myName()
+// console.log(name)
 
 
-// var name = 'riajul islam';
-// function myName(){
-//     console.log(name)
-// }
-// myName()
-
-let name = 'riajul islam';
-function myName(){
-    console.log(name)
+function myApp(){
+    if(true){
+        var firstName = 'riajul';
+        console.log(firstName)
+    }
+    if(true){
+        console.log(firstName)
+    }
+    console.log(firstName)
 }
-myName()
+myApp()

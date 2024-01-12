beee9e4cb24b89f963b94516ae8cbdc56d8bd3e2
Riajul Islam <riajulpislam@gmail.com>
2024-01-13 00:46:18 +0600

    update

--- Promise/ES6Module/ES6-Modules.js
+++ Promise/ES6Module/ES6-Modules.js
@@ -1,9 +1,6 @@
-//es6 modules
 
-//import { firstName } from './fName';
+import { firstName } from './utility/fName';
 
-import { Person } from './person';
-const person = new Person('riajul','imadul',22)
-person.info()
-console.log(person)
-//console.log(firstName)
+
+
+console.log(firstName)
